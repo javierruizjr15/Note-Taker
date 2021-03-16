b4490db78cfd3065dbda1a36e4de34c48ab09ade
Javier Ruiz <javierruizjr15@gmail.com>
2021-03-15 17:33:07 -0700

    done

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ app.use(express.static(join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.use(require('./public/routes'))
+app.use(require('./routes'))
 
 app.listen(process.env.PORT || 3000)
